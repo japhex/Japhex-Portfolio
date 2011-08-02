6280b09649cfa16349de68d25cf32e8433788eff
Jamie Lawrence <j@japhex.net>
2011-08-02 20:48:12 +0100

    Attempt to stop spam

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+  before_create :validate
+  
+  def validate
+    if comment.include?('viagra') || name.include?('viagra') || email.include?('viagra')
+      return false
+    end 
+  end
 end
